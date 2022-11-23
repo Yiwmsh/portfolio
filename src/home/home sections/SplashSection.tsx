2d0d1391cbc5698c8ebc6d54582cefb099ad8dcf
yiwmsh <lonelevelsands@gmail.com>
2022-11-23 16:44:01 -0700

    ✨Animated background and splash

---
+++ src/home/home sections/SplashSection.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ScrollSection } from "@yiwmsh/react-carpentry";
+import { motion } from "framer-motion";
+import styled from "@emotion/styled";
+
+export const SplashSection: React.FC = () => {
+  return <ScrollSection></ScrollSection>;
+};
--- src/home/home.tsx
+++ src/home/home.tsx
@@ -3,16 +3,29 @@ import { Button, Navbar, ScrollSnapper } from "@yiwmsh/react-carpentry";
 import React from "react";
 import { Modal } from "../components/Modal";
 import { IntrocutionSection } from "./home sections/IntroductionSection";
+import { SplashSection } from "./home sections/SplashSection";
 import { WelcomeModal } from "./home sections/WelcomeModal";
 import { YoutubeSection } from "./home sections/YoutubeSection";
+import { motion } from "framer-motion";
 
 export const NameContext = React.createContext({
   name: localStorage.getItem("name") ?? "Friend",
   setName: (value: string) => {},
 });
 
-const StickyNav = styled(Navbar)`
-  bottom: 0px;
+const BackgroundAnimation = styled(motion.div)`
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 200%;
+  background: radial-gradient(
+    circle at center top,
+    rgba(246, 217, 89, 1) 0%,
+    rgba(208, 124, 80, 1) 10%,
+    rgba(100, 60, 86, 1) 22.5%,
+    rgba(39, 36, 57, 1) 32.5%,
+    rgba(13, 15, 30, 1) 50%
+  );
 `;
 
 export const Home: React.FC = () => {
@@ -20,17 +33,29 @@ export const Home: React.FC = () => {
     localStorage.getItem("name") ?? "Friend"
   );
 
+  document.body.style.overflow = "hidden";
+  document.body.style.backgroundColor = `linear-gradient(
+    rgba(39, 36, 57, 1) 0%,
+    rgba(13, 15, 30, 1) 10%
+  )`;
+
   return (
     <>
+      <BackgroundAnimation
+        initial={{ top: "-100vh" }}
+        animate={{ top: 0 }}
+        transition={{ duration: 5 }}
+      />
       <NameContext.Provider value={{ name, setName }}>
         <WelcomeModal />
         <ScrollSnapper>
+          <SplashSection />
           <IntrocutionSection />
-          <StickyNav>
+          {/* <StickyNav>
             <Button onPress={() => localStorage.clear()}>
               Clear Local Storage (dev)
             </Button>
-          </StickyNav>
+          </StickyNav> */}
           <YoutubeSection />
           <IntrocutionSection />
         </ScrollSnapper>
