5a806b942a3d044c2c2f1a1702184f43db602ea0
yiwmsh <lonelevelsands@gmail.com>
2022-11-23 19:02:41 -0700

    ✨ Sun animation

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -15,7 +15,7 @@ export const NameContext = React.createContext({
 
 const BackgroundAnimation = styled(motion.div)`
   position: absolute;
-  z-index: -1;
+  z-index: -2;
   width: 100%;
   height: 200%;
   background: radial-gradient(
@@ -39,13 +39,99 @@ export const Home: React.FC = () => {
     rgba(13, 15, 30, 1) 10%
   )`;
 
+  const Canvas = styled(motion.svg)`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  `;
+
+  const BACKGROUND_TRANSITION = { duration: 5 };
+
+  const SUN_TRANSITION = {
+    type: "spring",
+    duration: 3,
+    delay: 2,
+    stiffness: 10,
+    damping: 5,
+    mass: 1,
+  };
+
+  const SUN_RAY_TRANSITION = {
+    type: "spring",
+    duration: 3,
+    delay: 2,
+    stiffness: 10,
+    damping: 5,
+    mass: 1,
+  };
+
+  const SUN_RAY_ANIMATION = {};
+
   return (
     <>
       <BackgroundAnimation
         initial={{ top: "-100vh" }}
         animate={{ top: 0 }}
-        transition={{ duration: 5 }}
+        transition={BACKGROUND_TRANSITION}
       />
+      <Canvas>
+        <defs>
+          <radialGradient id="sun">
+            <stop offset="0%" stop-color="rgba(208, 124, 80, 1)" />
+            <stop offset="100%" stop-color="#f4f812" />
+          </radialGradient>
+        </defs>
+        <motion.circle
+          fill={"url('#sun')"}
+          initial={{ x: "50%", r: 50, y: "-100vh" }}
+          animate={{ y: 10 }}
+          transition={SUN_TRANSITION}
+        />
+        <motion.path
+          d="M 3 5 L 5 7 L 5 4 Q 4 5 3 5"
+          fill="#f4f812"
+          initial={{ x: "45.5%", scale: 15, y: "-100vh", rotate: 98 }}
+          animate={{ y: 15 }}
+          transition={SUN_RAY_TRANSITION}
+          whileHover={{ scale: 20 }}
+        />
+        <motion.path
+          d="M 3 5 L 5 7 L 5 4 Q 4 5 3 5"
+          fill="#f4f812"
+          initial={{ x: "46.8%", scale: 15, y: "-100vh", rotate: 70 }}
+          animate={{ y: 56 }}
+          transition={SUN_RAY_TRANSITION}
+          whileHover={{ scale: 20 }}
+        />
+        <motion.path
+          d="M 3 5 L 5 7 L 5 4 Q 4 5 3 5"
+          fill="#f4f812"
+          initial={{ x: "49.5%", scale: 15, y: "-100vh", rotate: 25 }}
+          animate={{ y: 75 }}
+          transition={SUN_RAY_TRANSITION}
+          whileHover={{ scale: 20 }}
+        />
+        <motion.path
+          d="M 3 5 L 5 7 L 5 4 Q 4 5 3 5"
+          fill="#f4f812"
+          initial={{ x: "52.2%", scale: 15, y: "-100vh", rotate: -15 }}
+          animate={{ y: 60 }}
+          transition={SUN_RAY_TRANSITION}
+          whileHover={{ scale: 20 }}
+        />
+        <motion.path
+          d="M 3 5 L 5 7 L 5 4 Q 4 5 3 5"
+          fill="#f4f812"
+          initial={{ x: "53.8%", scale: 15, y: "-100vh", rotate: -44 }}
+          animate={{ y: 26 }}
+          transition={SUN_RAY_TRANSITION}
+          whileHover={{ scale: 20 }}
+        />
+      </Canvas>
+
       <NameContext.Provider value={{ name, setName }}>
         <WelcomeModal />
         <ScrollSnapper>
