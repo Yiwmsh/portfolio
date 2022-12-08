dbfa9cee39a6cc0aef097b72be5c193f053d869f
yiwmsh <lonelevelsands@gmail.com>
2022-12-08 12:42:34 -0700

    ✨ Routing added

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { LightTheme } from "./consts/index";
+import { routs } from "./router";
+import { Router, Outlet, ReactLocation } from "@tanstack/react-location";
 import { Home } from "./pages/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const location = new ReactLocation();
+
 root.render(
   <React.StrictMode>
     <ThemeContext theme={LightTheme}>
-      <Home />
+      {/* <Home /> */}
+      <Router routes={routs} location={location}>
+        <Outlet />
+      </Router>
     </ThemeContext>
   </React.StrictMode>
 );
---
+++ src/router.tsx
@@ -0,0 +1,8 @@
+import { Home } from "./pages";
+
+export const routs = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
