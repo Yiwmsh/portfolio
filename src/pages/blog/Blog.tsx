7b7df079c86e48a0fc0d00b464823eb0a979a7a5
yiwmsh <lonelevelsands@gmail.com>
2023-01-14 16:20:51 -0700

    🐛 Fixed undefined firebase search

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -29,7 +29,9 @@ export const Blog: React.FC = () => {
       setBlogPost(post);
     };
 
-    getPost();
+    if (blogSlug !== undefined) {
+      getPost();
+    }
   }, []);
   return (
     <ThemeContext theme={LightTheme}>
