798ef1d337cf7260391262f30a7de46e3b75a2db
yiwmsh <lonelevelsands@gmail.com>
2023-01-16 13:43:03 -0700

    🐛 Mobile style fixes for blog posts

---
+++ src/pages/blog/BlogPost/DateData.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Timestamp } from "firebase/firestore";
+import { fancyDisplayTimestamp } from "./BlogPost";
+import {
+  ScreenMaxWidth,
+  ScreenMinWidth,
+} from "../../../components/MediaQueries";
+
+const ScreenWidthBreakpoints = {
+  dateData: 630,
+};
+
+const DateDataBase = styled.ul`
+  font-size: 0.8em;
+  list-style: none;
+  display: inline-flex;
+
+  & > li {
+    margin: 5px;
+  }
+`;
+
+const NormalDateData = styled(DateDataBase)`
+  ${ScreenMaxWidth(
+    ScreenWidthBreakpoints.dateData,
+    `
+    display: none;
+`
+  )}
+`;
+
+const SmallScreenDateData = styled(DateDataBase)`
+  display: none;
+  margin: 0 auto;
+  padding: 0;
+  ${ScreenMaxWidth(
+    ScreenWidthBreakpoints.dateData,
+    `
+    display: inline-flex;
+`
+  )}
+`;
+
+const PublishedDate = styled.li``;
+
+const UpdatedDate = styled.li``;
+
+export const DateData: React.FC<{
+  publishedDate?: Timestamp | null;
+  lastUpdated?: Timestamp;
+  smallScreen?: boolean;
+}> = ({ publishedDate, lastUpdated, smallScreen }) => {
+  const dataInterior = (
+    <>
+      <PublishedDate>
+        Published {fancyDisplayTimestamp(publishedDate ?? Timestamp.now())}
+      </PublishedDate>
+      <li>&#183;</li>
+      <UpdatedDate>
+        Last Updated {fancyDisplayTimestamp(lastUpdated ?? Timestamp.now())}
+      </UpdatedDate>
+    </>
+  );
+
+  return (
+    <>
+      {smallScreen ? (
+        <SmallScreenDateData>{dataInterior}</SmallScreenDateData>
+      ) : (
+        <NormalDateData>{dataInterior}</NormalDateData>
+      )}
+    </>
+  );
+};
