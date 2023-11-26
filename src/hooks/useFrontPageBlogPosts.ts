fb92b2b2ac72667c96dba821648af7c0d70eb58f
yiwmsh <lonelevelsands@gmail.com>
2023-11-26 15:44:52 -0700

    Small bug fix

--- src/hooks/useFrontPageBlogPosts.ts
+++ src/hooks/useFrontPageBlogPosts.ts
@@ -4,7 +4,7 @@ import { getFrontPageBlogPosts } from "../components/BlogPostTools";
 export const useFrontPageBlogPosts = () =>
   useInfiniteQuery(
     "posts",
-    async ({ pageParam = 0 }) => {
+    async ({ pageParam = undefined }) => {
       return getFrontPageBlogPosts(15, pageParam);
     },
     {
