c0d011a77895b6522b6897d228d7be9d030815a0
yiwmsh <lonelevelsands@gmail.com>
2026-01-05 17:14:19 -0700

    Allow flipping flip cards

--- src/pages/scryfall querier/CardsGrid.tsx
+++ src/pages/scryfall querier/CardsGrid.tsx
@@ -105,6 +105,32 @@ export const CardsGrid: React.FC<CardsGridProps> = ({ cards }) => {
   );
 };
 
+const CardFlipButton: React.FC = () => {
+  return (
+    <svg
+      width={15}
+      height={15}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M4 10H17C19.2091 10 21 11.7909 21 14V14C21 16.2091 19.2091 18 17 18H12"
+        stroke="#000000"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7 6L3 10L7 14"
+        stroke="#000000"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
 interface CardDisplayProps {
   card: ScryfallCard;
 }
@@ -113,6 +139,8 @@ const CardDisplay: React.FC<CardDisplayProps> = ({ card }) => {
   const [activeFace, setActiveFace] = React.useState(0);
   const isMultifaced = isMultiFaced(card);
   const cardFaces = getCardFaces(card);
+  const [flipped, setFlipped] = React.useState(false);
+  console.log(card.layout);
 
   const imageUrl = React.useMemo(() => {
     if (isMultifaced) {
@@ -145,6 +173,7 @@ const CardDisplay: React.FC<CardDisplayProps> = ({ card }) => {
       <img
         style={{
           height: CARD_IMAGE_HEIGHT,
+          transform: flipped ? "rotate(180deg)" : "",
         }}
         key={card.id}
         src={imageUrl}
@@ -170,27 +199,26 @@ const CardDisplay: React.FC<CardDisplayProps> = ({ card }) => {
             }
           }}
         >
-          <svg
-            width={15}
-            height={15}
-            viewBox="0 0 24 24"
-            fill="none"
-          >
-            <path
-              d="M4 10H17C19.2091 10 21 11.7909 21 14V14C21 16.2091 19.2091 18 17 18H12"
-              stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M7 6L3 10L7 14"
-              stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <CardFlipButton />
+        </button>
+      )}
+      {card.layout === "flip" && (
+        <button
+          style={{
+            position: "absolute",
+            borderRadius: "100%",
+            cursor: "pointer",
+            top: `calc(${CARD_PADDING} + ( ${CARD_IMAGE_HEIGHT} * 0.92 ) )`,
+            left: "50%",
+            transform: "translate(-50%, 0)",
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setFlipped(!flipped);
+          }}
+        >
+          <CardFlipButton />
         </button>
       )}
       <div
